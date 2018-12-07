1d7dc173e07e649236f4531dd5134ef096326a9c
Vladimir Nachbaur <vizigr0u@gmail.com>
2018-12-07 00:47:42 -0500

    no longer try to draw outside of texture

--- UnityProject/Assets/Scripts/DrawOnTexture.cs
+++ UnityProject/Assets/Scripts/DrawOnTexture.cs
@@ -8,9 +8,9 @@ using UnityEngine.UI;
 public class DrawOnTexture : MonoBehaviour, IPointerClickHandler, IDragHandler
 {
     // public Texture2D baseTexture;
-    public int brushSize = 1; // size in pixels - TODO respect max
+    public int brushSize = 4; // size in pixels - TODO respect max
 
-    readonly int maxBrushSize = 4;
+    readonly int maxBrushSize = 25;
     Color DefaultColor => Color.black;
 
     Camera m_Camera;
@@ -42,6 +42,12 @@ public class DrawOnTexture : MonoBehaviour, IPointerClickHandler, IDragHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        List<RaycastResult> results = new List<RaycastResult>();
+        m_Raycaster.Raycast(eventData, results);
+        if (results.Any(r => r.gameObject == gameObject))
+        {
+            ApplyBrushAtPosition(eventData.position);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -50,32 +56,70 @@ public class DrawOnTexture : MonoBehaviour, IPointerClickHandler, IDragHandler
         m_Raycaster.Raycast(eventData, results);
         if (results.Any(r => r.gameObject == gameObject))
         {
-            Vector2 localpoint;
-            var rectTransform = GetComponent<RectTransform>();
+            ApplyBrushAtPosition(eventData.position);
+        }
+    }
 
-            if (m_Camera == null)
-                m_Camera = GetComponentInParent<Canvas>().worldCamera;
+    private void ApplyBrushAtPosition(Vector2 worldPosition)
+    {
+        Vector2 localpoint;
+        var rectTransform = GetComponent<RectTransform>();
 
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, m_Camera, out localpoint);
+        if (m_Camera == null)
+            m_Camera = GetComponentInParent<Canvas>().worldCamera;
 
-            Vector2 normalizedPoint = Rect.PointToNormalized(rectTransform.rect, localpoint);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, worldPosition, m_Camera, out localpoint);
 
-            Texture2D tex = m_TextureToDraw;
-            Vector2Int pixelUV = new Vector2Int((int)(normalizedPoint.x * tex.width), (int)(normalizedPoint.y * tex.height));
+        Vector2 normalizedPoint = Rect.PointToNormalized(rectTransform.rect, localpoint);
 
-            //for (int i = 0; i < tex.width; i++)
-            //{
-            //    for (int j = 0; j < tex.height; j++)
-            //    {
-            //        tex.SetPixel(i, j, Color.Lerp(Color.red, Color.Lerp(Color.blue, Color.green, (float) j / tex.height), (float) i / tex.height));
-            //    }
-            //}
+        Texture2D tex = m_TextureToDraw;
+        Vector2Int pixelUV = new Vector2Int((int)(normalizedPoint.x * tex.width), (int)(normalizedPoint.y * tex.height));
 
-            // TODO fix out of bounds
+        int halfBrush = brushSize / 2;
+        RectInt brushRect = new RectInt(pixelUV.x - halfBrush, pixelUV.y - halfBrush, brushSize, brushSize);
 
-            var halfBrushSize = brushSize / 2;
-            tex.SetPixels(pixelUV.x - halfBrushSize, pixelUV.y - halfBrushSize, brushSize, brushSize, blackColors);
-            tex.Apply();
+        ResizeRectToFit(ref brushRect, new RectInt(0, 0, tex.width, tex.height));
+        tex.SetPixels(brushRect.xMin, brushRect.yMin, brushRect.width, brushRect.height, blackColors);
+        tex.Apply();
+    }
+
+    private void DebugFillTexWithGradient(Texture2D tex)
+    {
+        for (int i = 0; i < tex.width; i++)
+        {
+            for (int j = 0; j < tex.height; j++)
+            {
+                tex.SetPixel(i, j, Color.Lerp(Color.red, Color.Lerp(Color.blue, Color.green, (float)j / tex.height), (float)i / tex.height));
+            }
+        }
+        tex.Apply();
+    }
+
+    private static void ResizeRectToFit(ref RectInt rect, RectInt constraint)
+    {
+        int leftOverflow = constraint.xMin - rect.xMin;
+        if (leftOverflow > 0)
+        {
+            rect.width -= leftOverflow;
+            rect.xMin = constraint.xMin;
+        }
+        int topOverflow = constraint.yMin - rect.yMin;
+        if (topOverflow > 0)
+        {
+            rect.height -= topOverflow;
+            rect.yMin = constraint.yMin;
+        }
+        int rightOverflow = rect.xMax - constraint.xMax;
+        if (rightOverflow > 0)
+        {
+            rect.width -= rightOverflow;
+            rect.xMax = constraint.xMax;
+        }
+        int bottomOverflow = rect.yMax - constraint.yMax;
+        if (bottomOverflow > 0)
+        {
+            rect.height -= bottomOverflow;
+            rect.yMax = constraint.yMax;
         }
     }
 }
